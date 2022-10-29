722773d9fc567c5d52564abca56b186e8b3b12e4
ahuja2666 <ahujaharshit26@gmail.com>
2022-10-29 17:12:20 +0530

    App component added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css"
 
 function App() {
+  const [data, setData] = useState([]);
+  const [value, setValue] = useState("")
+  const [copy, setCopy] = useState(data)
+
+  const setAll = (data) => {
+    setData(data);
+    setCopy(data);
+  }
+
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then(response => response.json())
+      .then(data => setAll(data));
+  }, []);
+
+  const handelSearch = () => {
+    let temp = data.filter((each) => {
+      return (each.title.includes(value))
+    })
+    setCopy(temp)
+    setValue("")
+
+  };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="parnet-cpntainer">
+        <section className="input-section">
+          <input className="s-input" placeholder="Search" onChange={(e) => setValue(e.target.value)} value={value} />
+          <button className="s-btn" onClick={() => handelSearch()}>Serach</button>
+        </section>
+        <section>
+          {
+            copy.map((each) => {
+              return (
+                <div key={each.id} className="card-conatiner">
+                  <h3>User id: {each.userId}</h3>
+                  <h4>Title: {each.title}</h4>
+                  <p><strong>Post:</strong> {each.body}</p>
+
+                </div>
+              )
+            })
+          }
+        </section>
+      </div>
+    </>
   );
 }
 
